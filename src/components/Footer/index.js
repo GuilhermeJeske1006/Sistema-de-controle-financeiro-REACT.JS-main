68dbb7757a6078a101c7e36ad811b68c6c91b675
GuilhermeJeske1006 <guilhermeieski@gmail.com>
2022-06-14 21:29:08 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as C from "./styles";
+
+const Footer = () => {
+  return (
+
+     <C.container>
+         <C.footer>
+             <C.title>
+                 Desenvolvido por Guilherme jeske
+             </C.title>
+         </C.footer>
+     </C.container>
+    
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+  height: 80px;
+  text-align: center;
+  background: #009e9f;
+  position: fixed;
+  width:100%;
+  left:0;
+  bottom:0;
+`;
+
+export const footer = styled.h3``;
+
+export const title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
